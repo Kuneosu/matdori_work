0b50838fc17d33ed3e7f6c7024b6586dcf36ab05
Kuneosu <kks92155784@gmail.com>
2023-07-17 17:49:53 +0900

    update_HOME

--- front/src/components/HomeCard.js
+++ front/src/components/HomeCard.js
@@ -11,7 +11,21 @@ const HomeCard = () => {
     return (
         <div className={`HOME_CARD ${expanded ? "expanded" : ""}`}
             onClick={handleClick}>
-
+            <div className="HOME_HEADER">
+                <div className="HOME_PROFILE"></div>
+                <div className="HOME_LINE"></div>
+                <div className="HOME_CONTENT_HEADER">
+                    <div className="HOME_CONTENT_TITLE">
+                        게시글 제목
+                    </div>
+                    <div className="HOME_CONTENT_DATE">
+                        2023.07.14
+                    </div>
+                </div>
+            </div>
+            <div className="HOME_CONTENT">
+                내용
+            </div>
         </div>
     );
 };
