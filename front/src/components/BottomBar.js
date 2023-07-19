295763500a4cacdc0ff912e64937ed8de0793911
Kuneosu <kks92155784@gmail.com>
2023-07-19 18:03:07 +0900

    마이 페이지 구현, 하단바 기능 업데이트

--- front/src/components/BottomBar.js
+++ front/src/components/BottomBar.js
@@ -1,12 +1,23 @@
 import "./BottomBar.css";
+import { useNavigate } from "react-router-dom";
 
 const BottomBar = () => {
+    const navigate = useNavigate();
+    const moveHome = () => {
+        navigate(`/home`);
+    }
+    const moveDic = () => {
+        navigate(`/dic`);
+    }
+    const moveMy = () => {
+        navigate(`/my`);
+    }
     return (
         <div className="BOTTOM_BAR">
-            <div className="BOTTOM_BAR_ICON FIRST_ICON"></div>
-            <div className="BOTTOM_BAR_ICON SECOND_ICON"></div>
-            <div className="BOTTOM_BAR_ICON THIRD_ICON"></div>
-        </div>
+            <button className="BOTTOM_BAR_ICON FIRST_ICON" onClick={moveHome}></button>
+            <button className="BOTTOM_BAR_ICON SECOND_ICON" onClick={moveDic}></button>
+            <button className="BOTTOM_BAR_ICON THIRD_ICON" onClick={moveMy}></button>
+        </div >
     );
 };
 
